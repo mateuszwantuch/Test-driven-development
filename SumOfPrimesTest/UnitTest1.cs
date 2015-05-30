59d899af8942dce45eb6fa4cac29ccf1247dedb3
Mateusz Wantuch <m.wantuch@hotmail.com>
2015-05-30 16:37:29 +0200

    Writing test

--- SumOfPrimesTest/UnitTest1.cs
+++ SumOfPrimesTest/UnitTest1.cs
@@ -9,6 +9,11 @@ namespace SumOfPrimesTest
         [TestMethod]
         public void TestMethod1()
         {
+            SumOfPrimes.Program testclass = new SumOfPrimes.Program();
+            ulong givenNumber = 20;
+            ulong expectResult = 77;
+            ulong result = testclass.SumOfPrimes(givenNumber);
+            Assert.AreEqual(result, expectResult);
         }
     }
 }
